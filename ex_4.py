33f90dd50c79b17a2004394cadb94c01f939d559
Mary Night <Poviraeva@me.com>
2016-12-23 00:36:44 +0300

    ex_4.py

---
+++ ex_4.py
@@ -0,0 +1,33 @@
+from librip.decorators import print_result
+
+# Необходимо верно реализовать print_result
+# и задание будет выполнено
+
+@print_result
+def test_1():
+    return 1
+
+
+@print_result
+def test_2():
+    return 'iu'
+
+
+@print_result
+def test_3():
+    return {'a': 1, 'b': 2}
+
+
+@print_result
+def test_4():
+    return [1, 2]
+
+@print_result
+def test_5(a,b,c=0):
+    return a+b+c
+
+test_1()
+test_2()
+test_3()
+test_4()
+test_5(2,2,c=2)
